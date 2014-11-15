59e72a0fb784d9b4e5dfb57ee86ad7b76418c5e1
funnythingz <funnythingz69+github@gmail.com>
2014-11-16 02:06:22 +0900

    added middleman-slim

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem "middleman", "~> 3.3.7"
 gem "middleman-blog", "~> 3.5.3"
 gem 'middleman-deploy', '~> 0.3.0'
 gem 'middleman-rouge'
+gem 'middleman-slim'
 
 # For feed.xml.builder
 gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -114,15 +114,12 @@ activate :deploy do |deploy|
   deploy.branch = 'master'
 end
 
-##
 # Markdown
-###
-
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
-###
 # Code highlighting
-###
-
 activate :rouge_syntax
+
+# slim
+set :slim, { :pretty => true, :sort_attrs => false, :format => :html5 }
