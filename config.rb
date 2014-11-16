6e256ef63f8c8c9976558849b82722c5075c6be2
funnythingz <funnythingz69+github@gmail.com>
2014-11-16 11:43:54 +0900

    tweaks

--- config.rb
+++ config.rb
@@ -112,7 +112,7 @@ end
 activate :deploy do |deploy|
   deploy.method = :git
   deploy.branch = 'master'
-  deploy.commit_message = 'deploy'
+  deploy.commit_message = "deploy - #{Time.new}"
   deploy.build_before = true
 end
 
